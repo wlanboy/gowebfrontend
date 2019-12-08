6a8940e2fb4109a19bbf74b51d8c60dc287d2545
wlanboy <me@wlanboy.com>
2019-12-08 22:24:02 +0100

    update to json properties

--- model/event.go
+++ model/event.go
@@ -8,24 +8,10 @@ import (
 type Event struct {
 	//gorm.Model
 	ID        uint64
-	UUID      string     `json:"uuid"`
+	UUID      string     `json:"uuid,omitempty"`
 	Name      string     `json:"name"`
 	Type      string     `json:"type"`
-	CreatedAt time.Time  `json:"created"`
-	UpdatedAt time.Time  `json:"updated"`
-	DeletedAt *time.Time `json:"deleted"`
-}
-
-/*Validate Event struct*/
-func (event *Event) Validate() (string, bool) {
-
-	if event.Name == "" {
-		return "name missing", false
-	}
-
-	if event.Type == "" {
-		return "Event Type missing", false
-	}
-
-	return "", true
+	CreatedAt time.Time  `json:"created,omitempty"`
+	UpdatedAt time.Time  `json:"updated,omitempty"`
+	DeletedAt *time.Time `json:"deleted,omitempty"`
 }
