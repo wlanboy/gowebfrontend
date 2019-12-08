e271b775a9d1436658072c76a325c7f2db9de2bf
wlanboy <me@wlanboy.com>
2019-12-08 22:25:16 +0100

    Added switch for in memory or http client based event store

--- application/indexhandler.go
+++ application/indexhandler.go
@@ -3,14 +3,24 @@ package application
 import (
 	"log"
 	"net/http"
+	"os"
 
+	httpclient "../httpclient"
+	model "../model"
 	store "../store"
 )
 
 /*GetAll GET method*/
 func (goservice *GoService) GetAll(w http.ResponseWriter, r *http.Request) {
 
-	datastore := store.GetInstance()
+	var datastore *model.SingletonEventStore
+
+	test := os.Getenv("TEST")
+	if test == "" {
+		datastore = httpclient.LoadEvents()
+	} else {
+		datastore = store.GetInstance()
+	}
 
 	err := goservice.Indexlayout.ExecuteTemplate(w, "layout", datastore)
 	if err != nil {
--- application/posthandler.go
+++ application/posthandler.go
@@ -3,7 +3,9 @@ package application
 import (
 	"log"
 	"net/http"
+	"os"
 
+	httpclient "../httpclient"
 	model "../model"
 	store "../store"
 )
@@ -13,12 +15,21 @@ func (goservice *GoService) PostCreate(w http.ResponseWriter, r *http.Request) {
 
 	Name := r.FormValue("Name")
 	Type := r.FormValue("Type")
+
 	newevent := model.Event{}
 	newevent.Name = Name
 	newevent.Type = Type
 
-	store.AddEvent(newevent)
-	datastore := store.GetInstance()
+	var datastore *model.SingletonEventStore
+
+	test := os.Getenv("TEST")
+	if test == "" {
+		httpclient.AddEvent(newevent)
+		datastore = httpclient.LoadEvents()
+	} else {
+		store.AddEvent(newevent)
+		datastore = store.GetInstance()
+	}
 
 	err := goservice.Indexlayout.ExecuteTemplate(w, "layout", datastore)
 	if err != nil {
