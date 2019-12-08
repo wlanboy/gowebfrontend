7b1572f8ad72ba7d22dd88bfb6eeefa18262c105
wlanboy <me@wlanboy.com>
2019-12-08 22:25:44 +0100

    Added first version of http client post call

---
+++ httpclient/eventclientpostevent.go
@@ -0,0 +1,30 @@
+package httpclient
+
+import (
+	"bytes"
+	"encoding/json"
+	"log"
+	"net/http"
+
+	model "../model"
+)
+
+/*AddEvent using event service*/
+func AddEvent(newevent model.Event) {
+
+	var netClient = ConfigureHTTPClient()
+
+	jsonValue, _ := json.Marshal(newevent)
+
+	req, reqerr := http.NewRequest("POST", "http://127.0.0.1:8000/api/v1/event", bytes.NewBuffer(jsonValue))
+
+	if reqerr != nil {
+		log.Println("HTTP Request error")
+		log.Fatal(reqerr)
+	} else {
+		req.Header.Set("Content-Type", "application/json")
+
+		response, _ := netClient.Do(req)
+		log.Println(response.StatusCode)
+	}
+}
